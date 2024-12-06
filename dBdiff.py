8482485b0a41cd727b944b1df8cd3848d2a515b6
gepe-hi <gp@HI-12108.ad.hi.no>
2024-12-06 10:31:01 +0100

    Added averaging

--- dBdiff.py
+++ dBdiff.py
@@ -1,24 +1,21 @@
 
-
 #
-# Assuming a dataset has been prepared
+# Assuming a dataset has been prepared and available (xarray/zarr)
 # i.e. the CRIMAC preprocessing pipeline has been run (raw->zarr)
-# Here we use a test data set (two files) from the annual NVG herring spawning survey
+# Here we use a test data set (two files) from the annual 2024 NVG herring spawning survey
 # The data includes clear herring schools midwater on the shelf outside Vesterålen
+#
 
-import xarray as xr
-import numpy as np
-import pandas as pd
 import os
-import json
 import sys
-import glob
+import xarray as xr
+import numpy as np
 import numpy as np
 import xarray as xr
-import scipy.io
 import matplotlib.pyplot as plt
 
 local = 1 # Running tests on local machine, 0->on IMR server
+average_data = 1 # 1->Average data, 0->No averaging
 
 # Example dataset with herring schools, two (crimac-scratch/test_data/dBDiff)
 # No preprocessing in Korona (ie averaging)
@@ -31,23 +28,25 @@ freqs=[38000, 200000]
 print(f)
 data=xr.open_dataset(f,engine="zarr")
 
-
 # Select a subset for testing
 dataSel=data.sv.sel(frequency=freqs,ping_time=slice("2024-02-21 05:00:00","2024-02-21 05:30:00"))
 
-# What's the best way to resample / coarsen prior to dB differencing and other operations in xarray????
-#dataSelResamp=dataSel.resample(ping_time="1min").mean(dim=["ping_time"]).coarsen(range=1, boundary="trim").mean()
-# That didn't work...
+if average_data==1:
+    dataSel=dataSel.resample(ping_time="1min").mean(dim=["ping_time"]).coarsen(range=5, boundary="trim").mean()
+    # Need a recent xarray version
+    # This is excrusiatingly slow isn't it???
 
 # Calculate the difference between the frequencies 38 and 200
 dB_diff = 10*np.log10(dataSel.sel(frequency=freqs[1])) - 10*np.log10(dataSel.sel(frequency=freqs[0]))
 
+# Select 38 kHz data and plot
+dB_38000=10*np.log10(dataSel.sel(frequency=38000))
+
+# Visualization of the data and results on differencing
+
 # Set min and max for visualization of Sv
 vmin=-82
 vmax=-30
-
-# Select 38 kHz data and plot
-dB_38000=10*np.log10(dataSel.sel(frequency=38000))
 
 fig, ax = plt.subplots(figsize=(10, 6))
 dB_38000.plot.pcolormesh(x='ping_time', y='range', ax=ax, cmap='viridis', vmin=vmin, vmax=vmax)
